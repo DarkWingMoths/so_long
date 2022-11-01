e0919644e02f313515872b40040b8d47888e2a24
DarkWingMoths <moths87@gmail.com>
2022-11-01 12:50:51 -0400

    push_04

--- main.c
+++ main.c
@@ -6,19 +6,29 @@
 /*   By: mgagnon <mgagnon@student.42quebec.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 11:26:49 by mgagnon           #+#    #+#             */
-/*   Updated: 2022/10/26 08:39:39 by mgagnon          ###   ########.fr       */
+/*   Updated: 2022/10/31 16:00:55 by mgagnon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* #include "so_long.h" */
-#include <mlx.h>
+/* #include "mlx.h" */
+#include "mlx_linux/mlx.h"
 
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	void	*asset_empty;
+	int	x;
+	int	y;
 
+	x = 32;
+	y = 32;
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 720, 576, "This is my window");
+	asset_empty = mlx_xpm_file_to_image(mlx, "./assets/Empty.xpm", &x, &y);
+	mlx_put_image_to_window(mlx, mlx_win, asset_empty, 0, 0);
+	mlx_put_image_to_window(mlx, mlx_win, asset_empty, 96, 0);
 	mlx_loop(mlx);
+	(void)mlx_win;
 }
